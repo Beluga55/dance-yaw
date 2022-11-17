f93e0d40207c4bd45946bfefa1c0405c7c05a87d
Beluga <beluga.55@gmail.com>
2022-11-17 21:38:36 +0800

    Done Project

--- js/main.js
+++ js/main.js
@@ -30,23 +30,59 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== IMAGES ===============*/
+const body = document.querySelector("body");
 const introToggle = document.querySelector(".intro__data");
 const introMenu = document.querySelector(".intro__popup");
-const introClose = document.querySelector(".intro__close");
-const body = document.querySelector("body");
+const introClose = document.querySelectorAll(".intro__close");
+const ScreenshotsPopup = document.querySelector(".screenshots__popup");
+const screenShotsImage = document.querySelectorAll(".screenshots__image");
+const PopupImg = document.querySelector(".screenshots__popup img");
+const DownloadButton = document.querySelectorAll(".screenshots__download");
 
 introToggle.addEventListener("click", () => {
   introMenu.classList.add("show");
   body.classList.add("scroll-lock");
 });
 
-introClose.addEventListener("click", () => {
-  introMenu.classList.remove("show");
-  body.classList.remove("scroll-lock");
+introClose.forEach((close) => {
+  close.onclick = () => {
+    introMenu.classList.remove("show");
+    body.classList.remove("scroll-lock");
+    ScreenshotsPopup.classList.remove("show");
+  };
+});
+
+/*==================== SCREENSHOTS ====================*/
+screenShotsImage.forEach((image) => {
+  image.onclick = () => {
+    ScreenshotsPopup.classList.add("show");
+    PopupImg.src = image.getAttribute("src");
+    body.classList.add("scroll-lock");
+
+    DownloadButton.forEach((download) => {
+      download.onclick = () => {
+        download.href = image.getAttribute("src");
+        download.download = image.getAttribute("id");
+      };
+    });
+  };
 });
 
 /*=============== SWIPER ABOUT AND SCREENSHOTS  ===============*/
 var swiperAbout = new Swiper(".about__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiperScreenshots = new Swiper(".screenshots__container", {
   spaceBetween: 32,
   grabCursor: true,
   centeredSlides: true,
@@ -81,3 +117,12 @@ modalCloses.forEach((modalClose) => {
     });
   });
 });
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
